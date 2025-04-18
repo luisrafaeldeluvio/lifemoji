a397520d63d99286e9fbfd5a4599256e0418d3d4
lrrd <github@lrrd.anonaddy.com>
2025-04-18 13:52:28 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
 import { fullName } from 'full-name-generator';
-import * as fs from 'fs';
 
-const prompt = msg => {
-  fs.writeSync(1, String(msg));
-  let s = '', buf = Buffer.alloc(1);
-  while(buf[0] - 10 && buf[0] - 13)
-    s += buf, fs.readSync(0, buf, 0, 1, 0);
-  return s.slice(1);
-};
-
-// const result = prompt('Input something: ');
-// console.log('Your input was: ' + result);
-
-class createNPC {
-    constructor() {
-
+class npc {  // npc extends stats?
+    constructor({
+        birthday = npc.newBirthday(),
+        sex = npc.newSex()
+    } = {}) {
+        this.id = npc.newId();
+        this.birthday = birthday;
+        this.sex = sex;
     }
 
-    rand(min, max) {
+    static randomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    newId() {
+    static newId() {
         const x = '1234567890abcdefghijklmnopqrstuvwxyz';
         let id = '';
         while (id.length < 16) {
-            id += x.charAt(this.rand(0, x.length));
+            id += x.charAt(npc.randomInt(0, x.length));
         }
         return id;
     }
 
-    newBirthday() {
+    static newBirthday() {
         const months = [
-            'January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agust', 'September', 'October', 'November', 'December'
+            'January', 'February', 'March', 'April', 
+            'May', 'June', 'July', 'Agust', 
+            'September', 'October', 'November', 'December'
         ]
-        return `${months[rand(1,12)]} ${rand(1,28)}`;
+        return `${months[npc.randomInt(0,11)]} ${npc.randomInt(1,28)}`;
     }
 
-    newSex()  {
-        return (this.rand(0, 100) < 49) ? "male" : "female";
+    static newSex()  {
+        return (npc.randomInt(0, 1) === 0) ? "male" : "female";
     }
     
 }
 
-function rand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const y = []
 
-console.log(fullName('US', 0));
+y.push(new npc({ sex: 'male'}))
+y.push(new npc())
+
+console.log(y);
+
