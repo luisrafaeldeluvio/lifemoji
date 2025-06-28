aa74f0d09778d950c5300d421f922c2e24e4abbc
lrrd <github@lrrd.anonaddy.com>
2025-06-28 08:09:21 +0800

    Made a seperate file for addNavListener.js.

---
+++ scripts/addNavListener.js
@@ -0,0 +1,30 @@
+function removeOpenMod() {
+    for (const element of document.querySelector(".main__journal").children) {
+        element.classList.remove("main__journal--opened");
+    }
+}
+
+function addNavListener() {
+    for (const element of document.querySelector(".main__secondary-nav ul")
+        .children) {
+        element.addEventListener("click", () => {
+            removeOpenMod();
+            document
+                .querySelector(`.main__journal__${element.id}`)
+                .classList.add("main__journal--opened");
+        });
+    }
+
+    for (const element of document.querySelector(".main__primary-nav ul")
+        .children) {
+        element.addEventListener("click", () => {
+            removeOpenMod();
+
+            document
+                .querySelector(`.main__journal__${element.id}`)
+                .classList.add("main__journal--opened");
+        });
+    }
+}
+
+export { addNavListener }
