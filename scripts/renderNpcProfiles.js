638b9470f02271238ea7c8769c01e8bb1ee2fe12
lrrd <github@lrrd.anonaddy.com>
2025-07-07 19:57:49 +0800

    Refactor NPC profile rendering to use dynamic data from relations

--- scripts/renderNpcProfiles.js
+++ scripts/renderNpcProfiles.js
@@ -1,26 +1,24 @@
-function renderNpcProfiles() {
-  console.log(true);
+import { readData, readDataByIndex, writeData } from "./data/dataManager.js";
 
+const npcList = await readDataByIndex("npc", "relations");
+
+async function renderNpcProfiles() {
   const parent = document.querySelector(".journal__social");
 
-  console.log(parent.children);
-
   for (let i of parent.querySelectorAll("div")) {
-    console.log(i);
-    let k = 0;
     for (let j = 0; j < 9; j++) {
       i.innerHTML += `
         <section class="profile profile--size-small card card--no-shadow">
           <img src="./assets/smart_car.jpg" alt="profile" />
           <div class="profile__text">
-            <span class="text--m js-profile">male, ${j + k}</span>
+            <span class="text--m js-profile">${npcList[j]}</span>
           </div>
         </section>`;
 
       // TODO: Add a click listener to each profile
       //       that opens a modal with more details about the profile.
+      console.log(npcList[0]);
     }
-    k++;
   }
 }
 
